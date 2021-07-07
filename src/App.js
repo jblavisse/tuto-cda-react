ceebe3fdb9e6bd5c313d4cdc7fc82039fce62d6b
jblavisse <jean-baptiste@jblavisse.fr>
2021-07-07 15:28:58 +0200

    Add Router in app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import './App.css';
-import Question from './Question';
-import Counter from './Counter';
-import Form from './Form';
-import UserForm from './UserForm';
-import CommentForm from './CommentForm';
-import Posts from './Posts';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import UsersList from './UsersList';
 import CommentsList from './CommentsList';
 
 function App() {
   return (
-    <div className="App">
-      {/* <Question title="Un titre" />
-      <Question title="Un autre titre" />
-      <Question title="Truc bidule chouette" />
-
-      <Counter />
-      <Counter />
-      
-      <Form />
-
-      <UserForm /> */}
-
-      {/* <CommentForm /> */}
-
-      {/* <Posts /> */}
-      <CommentsList />
-
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/users" component={UsersList} />
+        <Route path="/comments" component={CommentsList} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/UsersList.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import React, {useEffect, useState} from 'react'
+
+export default function UsersList() {
+    const [users,setUsers] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/users")
+        .then(res => setUsers(res.data));
+    }, [])
+
+    return (
+        <div>
+            <h1>Liste des utilisateurs</h1>
+            <ul>
+                {users.map(user => 
+                    <li>
+                        {user.name}({user.email})
+                    </li>
+                )}
+                
+            </ul>
+        </div>
+    )
+}
